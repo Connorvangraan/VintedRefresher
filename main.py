c0a4b186835ab0db18f7d0f927ac8a3dd5ea4057
Connor <connor.vangraan@gmail.com>
2023-05-22 21:44:36 +0100

    Accessed login page and enter details

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.common.keys import Key
 
 class VintedHandler:
     def __init__(self, username:str, password:str):
@@ -13,7 +14,41 @@ class VintedHandler:
         element = driver.find_element(By.ID, "onetrust-reject-all-handler")
         element.click()
 
-        time.sleep(1000)
+        time.sleep(1)
+        # u-cursor-pointer
+        buttons = driver.find_elements(By.CLASS_NAME, "u-cursor-pointer")
+        login_button = buttons[1]
+        login_button.click()
+
+        buttons = driver.find_elements(By.CLASS_NAME, "u-cursor-pointer")
+        email_button = buttons[0]
+        email_button.click()
+
+        fields = driver.find_elements(By.CLASS_NAME, "web_ui__Input__value")
+        username_field = fields[0]
+        username_field.send_keys(username)
+
+        password_field = fields[1]
+        password_field.send_keys(password)
+        password_field.submit()
+
+        #password_field.send_keys(password)
+        # time.sleep(1000)
+        # buttons = driver.find_elements(By.TAG_NAME, 'button')
+        # for b in buttons:
+        #     print(b.text)
+        #     print(b.id)
+        #     print(b.accessible_name)
+        #     print(b.tag_name)
+        #     print()
+        #
+        # continue_button = list(filter(lambda x: x.text == "Continue", buttons))
+        # print(continue_button)
+        # continue_button = continue_button[0]
+        # continue_button.click()
+        #
+        # wait = WebDriverWait(driver, 10)
+        time.sleep(100)
         driver.close()
 
 def main(user, passw):
